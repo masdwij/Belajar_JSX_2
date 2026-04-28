6be578aea89640374c0fc7de976d826c5150f129
Aurelino Stanta Anka <MasdwijGibus@gmail.com>
2026-04-28 09:12:43 +0700

    first commit

--- src/Exercise.jsx
+++ src/Exercise.jsx
@@ -8,16 +8,45 @@ function Exercise(props) {
     <>
       <div className="min-h-screen bg-gray-100 p-6">
         <h1 className="text-3xl font-bold text-center mb-6 text-blue-700">
-          User Cards
+          Postcard
         </h1>
         <div className="grid md:grid-cols-5 lg:grid-cols-5 gap-6 max-w-5x3 mx-auto">
-          <Postcard/>
-          <Postcard/>
-          <Postcard/>
-          <Postcard/>
-          <Postcard/>
-          <Postcard/>
-          <Postcard/>
+          <Postcard
+           title="Walls of Babylon"
+           description="The walls of Babylon were the city walls surrounding the ancient Mesopotamian city of Babylon, the political and religious centre of the Neo-Babylonian Empire. The walls of Babylon were included in many early versions of the Seven Wonders of the Ancient World."
+          />
+          <Postcard
+            title="Egyptian pyramids"
+            description="The Egyptian pyramids are ancient masonry structures located in Egypt. Most were built as tombs for the pharaohs and their consorts during the Old and Middle Kingdom periods. At least 138 identified pyramids have been discovered in Egypt. Approximately 80 pyramids were built within the Kingdom of Kush, now located in the modern country of Sudan."
+          />
+          <Postcard
+            title="Mausoleum at Halicarnassus"
+            description="The Mausoleum at Halicarnassus or Tomb of Mausolus (Ancient Greek: Μαυσωλεῖον τῆς Ἁλικαρνασσοῦ; Turkish: Halikarnas Mozolesi) was a tomb built between 353 and 351 BC in Halicarnassus (present Bodrum, Turkey) for Mausolus, an Anatolian from Caria and a satrap in the Achaemenid Persian Empire, and his sister-wife Artemisia II."
+          />
+          <Postcard
+            title="Colossus of Rhodes"
+            description="The Colossus of Rhodes (Ancient Greek: ὁ Κολοσσὸς Ῥόδιος, romanized: ho Kolossòs Rhódios; Modern Greek: Κολοσσός της Ρόδου, romanized: Kolossós tis Ródou) was a statue of the Greek sun god Helios, erected in the city of Rhodes, on the Greek island of the same name, by Chares of Lindos in 280 BC."
+          />
+          <Postcard
+            title="Temple of Artemis"
+            description="The Temple of Artemis or Artemision (Greek: Ἀρτεμίσιον; Turkish: Artemis Tapınağı), also known as the Temple of Diana, was a Greek temple dedicated to a localised form of the goddess Artemis (equated with the Roman goddess Diana)."
+          />
+          <Postcard
+            title="Statue of Zeus at Olympia"
+            description="Zeus is the sky and thunder god in ancient Greek religion, who rules as king of the gods on Mount Olympus."
+          />
+          <Postcard
+            title="Cyrus the Great"
+            description="Cyrus II of Persia[b] (c.600 -530 BC), commonly known as Cyrus the Great, was the founder of the Achaemenid Empire."
+          />
+          <Postcard
+            title=""
+            description=""
+          />
+          <Postcard
+            title=""
+            description=""
+          />
         </div>
       </div>
     </>
--- src/Postcard.jsx
+++ src/Postcard.jsx
@@ -5,12 +5,12 @@ function Postcard(props) {
     const [clicked, setClicked] = React.useState(false);
 
   return (
-    <div className="bg-white p-6 rounded-lg shadow hover:shadow-md transition  flex flex-col items-center">
-        <h2 className="text-xl font-semibold text-gray-800 mb-2 ">{title}</h2>
-      <p className="text-gray-600 ">
+    <div className="bg-white p-6 rounded-lg shadow hover:border hover:border-black hover:bg-red-100   flex flex-col items-center">
+        <h2 className="text-xl font-semibold text-gray-800 mb-2 text-center">{title}</h2>
+      <p className="text-gray-600 text-center">
         <span className="font-medium">Description:</span>{description}
       </p>
-      <button className={`${clicked ? "bg-special-red2" : "bg-gray-01"} text-white p-2 rounded-md`} onClick={() => setClicked(true)} >
+      <button className={`${clicked ? "bg-special-red2 hover:bg-special-red" : "bg-gray-01 hover:bg-gray-02"} text-white p-2 inline-full rounded-md`} onClick={() => setClicked(true)} >
         {clicked ? "Tombol sudah diklik" : "Silakan Klik"}
        </button>
     </div>
